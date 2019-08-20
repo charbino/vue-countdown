1d72fbd5772cf0e554a4e2811510a4fc23af3e6f
Sébastien Framinet <sebastien.framinet@asdoria.com>
2019-08-20 11:18:46 +0200

    firstCommit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="app-trip">
+        <h1 class="title">{{title}}</h1>
+        <Countdown :date="dateBegin"></Countdown>
+    </div>
+</template>
+
+<script>
+    import Countdown from './components/Countdown.vue';
+
+    export default {
+        name: 'Trip',
+        components: {
+            Countdown
+        },
+        data() {
+            return {
+                dateBegin: "2020-09-11 06:00:00",
+                title: "#Perou2020",
+            }
+        },
+    }
+</script>
+
+<style>
+    @import "../src/assets/font/trip.css";
+
+    .app-trip {
+        align-items: center;
+        bottom: 0;
+        background-color: #34495e;
+        /*display: flex;*/
+        /*justify-content: center;*/
+        left: 0;
+        position: absolute;
+        right: 0;
+        top: 0;
+    }
+
+    .countdown {
+        display: flex;
+        justify-content: center;
+    }
+
+    .block {
+        display: flex;
+        flex-direction: column;
+        margin: 20px;
+    }
+
+    .text {
+        color: #1abc9c;
+        font-size: 25px;
+        font-family: 'Roboto Condensed', serif;
+        font-weight: 35;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        text-align: center;
+    }
+
+    .digit {
+        color: #ecf0f1;
+        font-size: 120px;
+        font-weight: 100;
+        font-family: 'Roboto', serif;
+        margin: 10px;
+        text-align: center;
+    }
+
+    .title {
+        color: #ecf0f1;
+        font-size: 140px;
+        font-weight: 100;
+        font-family: 'Roboto', serif;
+        margin: 50px;
+        text-align: center;
+    }
+
+</style>
